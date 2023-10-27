46759d15f2b406d587b6277773197d8ca5fce3f8
JohanaLl <johisllanos@gmail.com>
2023-10-26 19:56:30 -0500

    Entrega 5: Se agrega la logica para recibir y guardar los mensajes del chat

--- Entregas/Entrega_5/src/app.js
+++ Entregas/Entrega_5/src/app.js
@@ -2,6 +2,7 @@ import express from "express";
 import productRouter from './routes/product.router.js';
 import cartRouter from './routes/cart.router.js';
 import usersRouter from './routes/users.router.js';
+import messagesRouter from './routes/messages.router.js';
 import viewsRouter from './routes/views.router.js';
 import { engine } from "express-handlebars"
 import { __dirname } from "./utils.js";
@@ -22,9 +23,27 @@ app.set("view engine", "handlebars");
 app.use("/api/products", productRouter);
 app.use("/api/carts", cartRouter);
 app.use("/api/users", usersRouter);
+app.use("/api/messages", messagesRouter);
 app.use("/", viewsRouter)
 
+const PORT = 8080
+const httpServer = app.listen(PORT, () => {
+    console.log(`Listening port ${PORT}`);
+})
 
-app.listen(8080, () => {
-    console.log('Listening port 8080');
+//sockerServer
+const sockerServer = new Server(httpServer);
+const messages = [];
+
+sockerServer.on('connetion', (socket) => {
+    console.log(`Customer connected: ${socket.id}`);
+
+    socket.on('newUser', (user) => {
+        socket.broadcast.emit("userConnected", user);
+        socket.emit('connected');
+    })
+    socket.on('message', info => {
+        messages.push(info);
+        sockerServer.emit('chat', messages)
+    })
 })
--- Entregas/Entrega_5/src/routes/messages.router.js
+++ Entregas/Entrega_5/src/routes/messages.router.js
@@ -3,4 +3,13 @@ import { messagesManager } from '../dao/managers/messagesManager.js'
 
 const router = Router();
 
+router.post('/', async (req, res) => {
+    try {
+        const response = await messagesManager.createOne(req.body)
+        return res.status(200).json({ message: "saved message"})
+    } catch (error) {
+        
+    }
+})
+
 export default router;
