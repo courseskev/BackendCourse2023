ebf5ed68fc1db578c0dc0ee7cae1a88fb9e5059f
Kevin <kevcourses0@gmail.com>
2023-11-06 21:12:32 -0500

    Entrega 6: Se hace los renders correspondientes de Login y Signup. También la lógica para manejar las sesiones.

---
+++ Entregas/Entrega_6/src/routes/sessions.router.js
@@ -0,0 +1,57 @@
+import { Router } from 'express'
+import { usersManager } from '../dao/managers/usersManager.js'
+
+const sessionRouter = Router()
+
+sessionRouter.post("/signup", async (req, res) => {
+    const { first_name, last_name, email, password, password2 } = req.body
+    if (!first_name || !last_name || !email || !password || !password2)
+        return res.status(400).json({ message: "All fields are mandatory" })
+    if (password !== password2)
+        return res.status(400).json({ message: "Password doesn't match" })
+    try {
+        const result = await usersManager.createOne(req.body)
+        //res.status(201).json({message: "user created", user: result})
+        console.log("User created:", result);
+        return res.redirect("/views/login")
+    } catch (err) {
+        res.status(500).json({ Error: err.message })
+    }
+})
+
+sessionRouter.post("/login", async (req, res) => {
+    const { email, password } = req.body
+    if (!email || !password)
+        return res.status(400).json({ message: "All fields are mandatory" })
+    try {
+        const result = await usersManager.findByEmail(email)
+        if (!result)
+            return res.redirect("/views/signup")
+
+
+        const isPasswordvalid = password === result.password
+        if (!isPasswordvalid) {
+            res.render('login', { error: 'Password is incorrect' });
+        }
+        else {
+            const sessionInfo =
+                email === "adminCoder@coder.com" && password === "adminCod3r123"
+                    ? { email, name: result.first_name+result.last_name, isAdmin: true }
+                    : { email, name: result.first_name+result.last_name, isAdmin: false };
+            req.session.user = sessionInfo
+            //req.session.user = { email, name: result.first_name + result.last_name }
+            res.redirect("/views/products")
+        }
+
+    } catch (err) {
+        res.status(500).json({ Error: err.message })
+    }
+})
+
+sessionRouter.get("/logout", (req, res) => {
+    req.session.destroy(() => {
+        res.redirect("/views/login")
+    })
+})
+
+export default sessionRouter
---
+++ Entregas/Entrega_6/src/routes/views.router.js
@@ -0,0 +1,46 @@
+import { Router } from "express";
+import { usersManager } from "../dao/managers/usersManager.js";
+import { productManager } from "../dao/managers/productManager.js";
+import { cartManager } from "../dao/managers/cartManager.js";
+
+const routerViews = Router();
+
+routerViews.get("/login", (req, res) => {
+    if(req.session.user)
+        return res.redirect("/views/products")
+    res.render("login");
+});
+
+routerViews.get("/signup", (req, res) => {
+    if(!req.session.user)
+        return res.redirect("/views/login")
+    res.render("signup");
+});
+
+routerViews.get("/chat", (req, res) => {
+    if(!req.session.user)
+        return res.redirect("/views/login")
+    res.render("chat");
+})
+
+
+routerViews.get("/products", async(req, res)=>{
+    if(!req.session.user)
+        return res.redirect("/views/login")
+    const result = await productManager.findAll(req.query);
+    const tmp = JSON.parse(JSON.stringify(result));    
+    const sessionUser =  req.session.user  
+    res.render("products", {products: tmp, user: sessionUser});
+})
+
+routerViews.get("/carts/:cid", async(req, res)=>{
+    if(!req.session.user)
+        return res.redirect("/views/login")
+    const { cid } = req.params;
+    const carts = await cartManager.findCartById(cid);        
+    const cleanData = JSON.parse(JSON.stringify(carts));    
+    res.render("cart", cleanData);
+})
+
+
+export default routerViews
